46494a9b7f3859e6aea0cbc629c8db0243e4664c
Hbrown777 <hbrown7530@gmail.com>
2026-04-11 22:00:55 -0500

    voltron comeback

--- SimpleSniffBackend/Program.cs
+++ SimpleSniffBackend/Program.cs
@@ -1,9 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -13,28 +12,21 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader());
 });
 
-app.UseCors("AllowAll");
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Middleware
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.UseCors("AllowFrontend");
 
 app.Run();
